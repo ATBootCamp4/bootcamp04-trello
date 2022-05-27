b60692490cbe046ba1b348a5d4a8583ee6c8c065
Eduardo Martínez <eduardomc_@outlook.com>
2022-05-27 17:46:01 -0500

    Fixed lint errors on a new branch

---
+++ tests/api/features/steps/cards_steps.py
@@ -0,0 +1,53 @@
+
+from behave import given, step
+from main.core.rest.request_manager import RequestManager
+request_manager = RequestManager()
+
+
+@given('I created a new card')
+def step_impl(context):
+    context.status_code, context.card = context.request_manager.post_request(
+        'cards', payload={'name': 'Behave card', 'idList': context.list['id']})
+
+
+@step('verify the comment has been added')
+def step_impl(context):
+    '''
+        This method verifies if a comment has been added to a card
+        by checking if its ID is present in the list of actions of a card
+    '''
+    comment_id = context.response['id']
+    card_id = context.response['data']['card']['id']
+    context.status_code, context.response = request_manager.do_request(
+        "GET", "cards/" + card_id + "/actions")
+    error_msg = f"Comment {comment_id} is not present on card {card_id}"
+    assert comment_id in [action['id']
+                          for action in context.response], error_msg
+
+
+@step('verify the comment has been deleted')
+def step_impl(context):
+    '''
+        This method verifies if a comment has been deleted from a card
+        by checking if its ID is not present in the list of actions of a card
+    '''
+    comment_id = context.comment['id']
+    card_id = context.card['id']
+    context.status_code, context.response = request_manager.do_request(
+        "GET", "cards/" + card_id + "/actions")
+
+    assert comment_id not in [action['id']
+                              for action in context.response], f"Comment {comment_id} is present on card {card_id}"
+
+
+@step('I created a comment in the card')
+def step_impl(context):
+    '''
+        This method creates comment in a card with some text.
+        It is used along with another steps and has no
+        major functionality
+    '''
+    context.status_code, context.comment = request_manager.do_request(
+        "POST", "cards/" + context.card['id'] + "/actions/comments", payload={"text": "InitialComment"})
+
+    assert context.status_code == 200, f"Couldn't create card, status code is {context.status_code}, expected 200"
--- tests/api/features/steps/common_steps.py
+++ tests/api/features/steps/common_steps.py
@@ -1,18 +1,14 @@
-from behave import given, then, step
+from behave import then, step
 from main.utils.behave_helpers import replace_ids, fill_payload, validate_schema
-
-
-@given('I created a new card')
-def step_impl(context):
-    _, context.card = context.request_manager.post_request(
-        'cards', payload={'name': 'Behave card', 'idList': context.list['id']})
+from main.utils.json_model import JsonModel
 
 
 @step('I send a "{method}" request to "{endpoint}"')
 def step_impl(context, method, endpoint):
     endpoint = replace_ids(context, endpoint)
     context.payload = fill_payload(context, payload={})
-    context.status_code, context.response = context.request_manager.do_request(method, endpoint, context.payload)
+    context.status_code, context.response = context.request_manager.do_request(
+        method, endpoint, context.payload)
 
 
 @then('I receive a list with at least "{quantity:d}" "{item}"')
@@ -34,3 +30,41 @@ def step_impl(context, status_code):
 @then('I receive a response with the "{schema_name}" schema')
 def step_impl(context, schema_name):
     validate_schema(context, schema_name)
+
+
+@ step('The item is updated')
+def step_impl(context):
+    '''
+        This method checks if a response item has been updated
+        by comparing its objetc properties from the response
+        to the input values in the request
+    '''
+    model = JsonModel()
+    for row in context.table:
+        model.build_json(row['Key'], row['Value'])
+    context.model = model
+    for key, value in model.json_data.items():
+        actual_value = context.response[key]
+        assert value == actual_value, f"Response Object Error: Expected: {value}, Actual: {actual_value}"
+
+
+@ step('The item "{item_url}" is deleted')
+def step_impl(context, item_url):
+    '''
+        This method checks if item has been deleted by
+        making a GET request to its URL
+    '''
+    endpoint = replace_ids(context, item_url)
+    status_code, _ = context.request_manager.do_request("GET", endpoint)
+    assert status_code == 404, f"Status code 404 was expected but {status_code} was found"
+
+
+@ step('The item "{item_url}" is created')
+def step_impl(context, item_url):
+    '''
+        This method checks if item has been created by
+        making a GET request to its URL
+    '''
+    endpoint = replace_ids(context, item_url)
+    status_code, _ = context.request_manager.do_request("GET", endpoint)
+    assert status_code == 200, f"Status code 200 was expected but {status_code} was found"
