7e33999bce25ac1f8cedfd7e26fe6b468c6d9ef7
Eduardo Martínez <eduardomc_@outlook.com>
2022-06-09 14:00:49 -0500

    Fixed flake8 error

--- tests/gui/features/steps/test_gui.py
+++ tests/gui/features/steps/test_gui.py
@@ -7,7 +7,6 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-#from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
 
 
